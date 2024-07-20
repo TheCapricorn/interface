d1d2d94fc2b55faf0533022ce01a642927cd3d6e
孙志奇 <769053649@qq.com>
2024-07-20 19:36:03 +0800

    feat:init

--- apps/web/src/i18n.tsx
+++ apps/web/src/i18n.tsx
@@ -21,7 +21,12 @@ i18n
       if (language === 'en-US') {
         return enUsLocale
       }
-      return import(`./i18n/locales/translations/${language}.json`)
+      try{
+        return  import(`./i18n/locales/translations/${language}.json`)
+      }catch{
+        return  import(`./i18n/locales/translations/CN.json`)
+      }
+
     }),
   )
   .on('failedLoading', (language, namespace, msg) => {
